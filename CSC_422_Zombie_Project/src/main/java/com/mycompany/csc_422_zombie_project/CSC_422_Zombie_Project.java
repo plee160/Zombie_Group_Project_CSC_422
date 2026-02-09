6c63a216ca4f393dc65b685bb258867da07e6cc3
plee160 <philip.hmoov.lee@gmail.com>
2026-02-09 17:17:15 -0600

    Add files via upload

---
+++ CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/CSC_422_Zombie_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.csc_422_zombie_project;
+
+/**
+ *
+ * @author Phili
+ */
+public class CSC_422_Zombie_Project {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
