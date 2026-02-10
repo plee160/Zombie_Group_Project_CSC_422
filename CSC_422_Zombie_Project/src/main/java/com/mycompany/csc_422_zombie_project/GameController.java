1a8093602a5474428d475ac244c6f479ba68bd1a
alex-filus <alex.salamonska@gmail.com>
2026-02-09 20:53:42 -0600

    Added notes

--- CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/GameController.java
+++ CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/GameController.java
@@ -9,5 +9,15 @@ package com.mycompany.csc_422_zombie_project;
  * @author Phili
  */
 public class GameController {
-    //testing 
+    //Main Menu - start the game
+    
+    //Generate a random list of survivors and zombies
+    
+    //Main game loop:
+    //survivor attacks zombie (subtract health from zombie)
+    //zombie attacks a survivor (subtract health from survivor)
+    //end loop when either zombies or survivors are dead
+    
+    //Generate a report:
+    //display who survived
 }
