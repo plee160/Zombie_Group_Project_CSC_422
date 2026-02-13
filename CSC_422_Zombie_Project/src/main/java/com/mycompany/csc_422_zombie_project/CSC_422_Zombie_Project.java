b8d13e5ed00d13bccc0e1380392364b73f22f704
katie-csp <baileyk7@csp.edu>
2026-02-13 15:55:17 -0600

    Update CSC_422_Zombie_Project.java

--- CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/CSC_422_Zombie_Project.java
+++ CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/CSC_422_Zombie_Project.java
@@ -1,10 +1,11 @@
 /*
- * Author: 
+ * Author: Kyson, Philip, Alex, and Katie
  * Date: 02/13/2026
  * Class: CSC 422 100
  * Project: Zombie Wars
- * File Name: CSC_422_Zombie_Project
- * Description: This is the main driver class for the Zombie Wars Game. It creates the GameController and starts the game.
+ * File Name: CSC_422_Zombie_Project.java
+ * Description: This is the main driver class for the Zombie Wars Game. 
+ * It creates the GameController and starts the game.
  *
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
