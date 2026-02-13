43d54feefbaf388f4050e1a9410d70a6ee1c1e0a
katie-csp <baileyk7@csp.edu>
2026-02-13 15:52:49 -0600

    Update commonInfectedCharacter.java

--- CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/commonInfectedCharacter.java
+++ CSC_422_Zombie_Project/src/main/java/com/mycompany/csc_422_zombie_project/commonInfectedCharacter.java
@@ -1,3 +1,11 @@
+/*
+ * Author: Kyson, Philip, Alex, and Katie
+ * Date: 02/13/2026
+ * Class: CSC 422 100
+ * Project: Zombie Wars
+ * File Name: commonInfectedCharacter.java
+ * Description: Represents a common infected zombie.
+ */
 package com.mycompany.csc_422_zombie_project;
 
 public class commonInfectedCharacter extends Zombie{
